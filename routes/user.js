546f6406c9dd8942f064f10825f192c64420a164
Iftekhar Uddin <iftekharuddin720@gmail.com>
2023-11-23 20:07:24 +0600

    second commit

--- routes/user.js
+++ routes/user.js
@@ -16,12 +16,12 @@ const router = express.Router();
 //     res.send("hello user, you are loged in and you can delete all accounts!")
 // })
 
-router.put("/:id", verifyUser, updateUser);
+router.put("/:id", updateUser);
 
-router.delete("/:id", verifyUser, deleteUser);
+router.delete("/:id", deleteUser);
 
-router.get("/:id", verifyUser, getUser);
+router.get("/:id", getUser);
 
-router.get("/",  verifyAdmin, getAllUser);
+router.get("/", getAllUser);
 
 export default router;
